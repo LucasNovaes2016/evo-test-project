406fe708bb059badb798ae79a4fff1f6869176c4
Lucas Novaes <lucasmsn200@gmail.com>
2020-11-15 11:13:57 -0300

    feat: adding header

---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function HeaderSection() {
+  return (
+    <header className="header-section">
+      <div className="bg-primary text-white p-2">
+        <div className="container">
+          <h1 className="text-center">
+            EVOLUX - TELECOM CARRIER PROJECT @2020
+          </h1>
+        </div>
+      </div>
+    </header>
+  );
+}
